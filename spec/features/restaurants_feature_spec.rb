58927709a271bb39ec0d4f2ff92ee0a54b882f93
Mishal Islam <mishal.islam01@gmail.com>
2014-12-16 18:07:05 +0000

    refactored tests

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
@@ -9,68 +10,58 @@ feature 'restaurants' do
     end
   end
 
-  context 'display restaurants' do
+  context 'with KFC in database' do
+
     before do
-      Restaurant.create(name: 'KFC')
-    end
-    scenario 'display restaurants' do
-      visit '/restaurants'
-      expect(page).to have_content('KFC')
-      expect(page).not_to have_content('No restaurants yet')
-    end
-  end
-
-  context 'creating restaurants' do
-    scenario 'prompts user to fill out a form, then displays the new restaurant' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-      expect(page).to have_content('KFC')
-      expect(current_path).to eq '/restaurants'
-    end
-  end
-
-  context 'viewing restaurants' do
-  	before do
-  		@kfc = Restaurant.create(name: 'KFC')
-  	end
-
-  	scenario 'lets a user view a restaurant' do
-  		visit '/restaurants'
-  		click_link 'KFC'
-  		expect(page).to have_content('KFC')
-  		expect(current_path).to eq("/restaurants/#{@kfc.id}")
-  	end
-  end
-
-  context 'editing restaurants' do
-    before do
-      Restaurant.create(name:'KFC')
+        @kfc = Restaurant.create(name: 'KFC')
     end
 
-    scenario "lets a user edit a restaurant" do
-      visit '/restaurants'
-      click_link 'Edit KFC'
-      fill_in 'Name', with: 'Kentucky Fried Chicken'
-      click_button 'Update Restaurant'
-      expect(page).to have_content 'Kentucky Fried Chicken'
-      expect(current_path).to eq '/restaurants'
+    context 'display restaurants' do
+      scenario 'display restaurants' do
+        visit '/restaurants'
+        expect(page).to have_content('KFC')
+        expect(page).not_to have_content('No restaurants yet')
+      end
+    end
+
+    context 'creating restaurants' do
+      scenario 'prompts user to fill out a form, then displays the new restaurant' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'McDonalds'
+        click_button 'Create Restaurant'
+        expect(page).to have_content('McDonalds')
+        expect(current_path).to eq '/restaurants'
+      end
+    end
+
+    context 'viewing restaurants' do
+    	scenario 'lets a user view a restaurant' do
+    		visit '/restaurants'
+    		click_link 'KFC'
+    		expect(page).to have_content('KFC')
+    		expect(current_path).to eq("/restaurants/#{@kfc.id}")
+    	end
+    end
+
+    context 'editing restaurants' do
+      scenario "lets a user edit a restaurant" do
+        visit '/restaurants'
+        click_link 'Edit KFC'
+        fill_in 'Name', with: 'Kentucky Fried Chicken'
+        click_button 'Update Restaurant'
+        expect(page).to have_content 'Kentucky Fried Chicken'
+        expect(current_path).to eq '/restaurants'
+      end
+    end
+
+    context 'deleting restaurants' do
+    	scenario 'removes a restaurant when user clicks delete link' do
+    		visit '/restaurants'
+    		click_link('Delete KFC')
+    		expect(page).not_to have_content('KFC')
+    		expect(page).to have_content('Restaurant deleted successfully')
+    	end
     end
   end
-
-  context 'deleting restaurants' do
-
-  	before do
-  		Restaurant.create(name: 'KFC')
-  	end
-
-  	scenario 'removes a restaurant when user clicks delete link' do
-  		visit '/restaurants'
-  		click_link('Delete KFC')
-  		expect(page).not_to have_content('KFC')
-  		expect(page).to have_content('Restaurant deleted successfully')
-  	end
-  end
-
 end
---
+++ spec/features/reviews_feature_spec.rb
@@ -0,0 +1,11 @@
+# require 'rails_helper'
+
+# feature 'reviewing' do
+  
+#   before do
+#     Restaurant.create(name: 'KFC')
+#   end
+
+  
+
+# end
