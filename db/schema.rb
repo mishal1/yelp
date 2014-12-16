5b9e1fd3b80a9cd31af888e986e3c228626352c3
Mishal Islam <mishal.islam01@gmail.com>
2014-12-16 18:52:22 +0000

    created test that checks users can leave a review

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141216170400) do
+ActiveRecord::Schema.define(version: 20141216184311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20141216170400) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.text     "thoughts"
+    t.integer  "rating"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
