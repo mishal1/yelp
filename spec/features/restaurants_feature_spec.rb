a30f1c8f2daa10e18eca3766bc75dcb39ef24ae6
langesi <langesi@aol.de>
2014-12-16 12:52:49 +0000

    have added a second test for displaying restaurants from DB

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -4,8 +4,20 @@ feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
-      expect(page).to have_content 'No restaurants'
+      expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
+  end
+
+  context 'display restaurants' do
+  	before do
+  		Restaurant.create(name: 'KFC')
+  	end
+
+  	scenario 'display restaurants' do
+  		visit '/restaurants'
+  		expect(page).to have_content('KFC')
+  		expect(page).not_to have_content('No restaurants yet')
+  	end
   end
 end
