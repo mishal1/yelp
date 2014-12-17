c323dc2c9598c0bf565a362d35db865bd54af5ea
Mishal Islam <mishal.islam01@gmail.com>
2014-12-17 12:24:49 +0000

    review is invalid if rating is greater than 5

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 feature 'restaurants' do
 
   context 'no restaurants have been added' do
+
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
+
   end
 
   context 'with KFC in database' do
@@ -17,14 +19,17 @@ feature 'restaurants' do
     end
 
     context 'display restaurants' do
+
       scenario 'display restaurants' do
         visit '/restaurants'
         expect(page).to have_content 'KFC'
         expect(page).not_to have_content 'No restaurants yet'
       end
+
     end
 
     context 'creating restaurants' do
+
       scenario 'prompts user to fill out a form, then displays the new restaurant' do
         visit '/restaurants'
         click_link 'Add a restaurant'
@@ -33,18 +38,22 @@ feature 'restaurants' do
         expect(page).to have_content 'McDonalds'
         expect(current_path).to eq '/restaurants'
       end
+
     end
 
     context 'viewing restaurants' do
+
     	scenario 'lets a user view a restaurant' do
     		visit '/restaurants'
     		click_link 'KFC'
     		expect(page).to have_content 'KFC'
     		expect(current_path).to eq "/restaurants/#{@kfc.id}"
     	end
+
     end
 
     context 'editing restaurants' do
+
       scenario "lets a user edit a restaurant" do
         visit '/restaurants'
         click_link 'Edit KFC'
@@ -53,17 +62,42 @@ feature 'restaurants' do
         expect(page).to have_content 'Kentucky Fried Chicken'
         expect(current_path).to eq '/restaurants'
       end
+
     end
 
     context 'deleting restaurants' do
+
     	scenario 'removes a restaurant when user clicks delete link' do
     		visit '/restaurants'
     		click_link 'Delete KFC'
     		expect(page).not_to have_content 'KFC'
     		expect(page).to have_content 'Restaurant deleted successfully'
     	end
+
+    end
+
+    context 'an invalid restaurant' do
+
+      scenario 'does not let you submit a name that is too short' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'kf'
+        click_button 'Create Restaurant'
+        expect(page).not_to have_css 'h2', text: 'kf'
+        expect(page).to have_content 'error'
+      end
+
+      scenario 'does not let you submit a restaurant if it is not unique' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'KFC'
+        click_button 'Create Restaurant'
+        expect(page).to have_content 'error'
+      end
+
     end
 
   end
+
   
 end
