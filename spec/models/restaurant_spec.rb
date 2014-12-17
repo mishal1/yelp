7e20b67bf2f4ac3801f39517379c21e18591c39c
Mishal Islam <mishal.islam01@gmail.com>
2014-12-17 11:34:28 +0000

    model test passed

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
-Rspec.describe Restaurant, type: :model do
+describe Restaurant, type: :model do
   it 'is not valid with a name of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
 end
