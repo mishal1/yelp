041541912f5e82017ae052d38d87525d7ebe1878
langesi <langesi@aol.de>
2014-12-18 16:28:12 +0000

    wrote a unit test for symbole star

--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -8,6 +8,11 @@ describe ReviewsHelper, type: :helper do
       expect(helper.star_rating('N/A')).to eq 'N/A'
     end
 
+    it 'should have symbole stars' do
+    	expect(helper.star_rating('5')).to eq '★★★★★'
+
+    end
+
   end
   
 end
